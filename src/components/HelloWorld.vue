5f6cff33af87b5ccea4d3c8d8313d71c19230b2a
Vladyslav Petruk <v.petruk@viseven.com>
2022-04-13 15:57:11 +0300

    add(*): structure, components

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="hello">
+    <button @click="addUser">Add User</button>
+  </div>
+</template>
+
+<script>
+import { mapActions } from "vuex";
+
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+  methods: {
+    ...mapActions("users", ["addNewUser"]),
+    addUser() {
+      const newUser = {
+        name: "Ivan",
+        age: 25,
+      };
+      this.addNewUser(newUser);
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style></style>
---
+++ src/store/modules/users.js
@@ -0,0 +1,31 @@
+import Vue from 'vue';
+
+const usersStore = {
+    namespaced: true,
+    state: {
+        list: {
+            1: {
+                name: 'Denis',
+                age: 30
+            }
+        }
+    },
+    getters: {
+        usersList: ({ list }) => Object.values(list)
+    },
+    mutations: {
+        ADD_USER(state, user) {
+            Vue.set(state.list, user.id, user)
+        }
+    },
+    actions: {
+        addNewUser({ commit }, user) {
+            const newUser = {
+                ...user, id: String(Math.random())
+            };
+            commit('ADD_USER', newUser);
+        }
+    }
+};
+
+export default usersStore;
